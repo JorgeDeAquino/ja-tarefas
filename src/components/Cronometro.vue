548f9afeab3e4fd10f8fff3cf689e84a4510dd1e
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-03-08 22:38:21 -0300

    ajustes do formulario

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <strong>
+            {{ tempoDcorrido }}
+        </strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSugundos: {
+            type: Number,
+            default: 0
+        }
+    },
+    computed: {
+        tempoDcorrido () : string {
+            return new Date(this.tempoEmSugundos * 1000).toISOString().substring(19,11)
+        }
+    },
+})
+</script>
